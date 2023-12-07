35f9968680f5d657b1caf61d3986d6edc4544170
Jonathan Maldonado Guemarez <cantor.dedekind@gmail.com>
2023-12-07 16:29:12 -0500

    Added cleanup function to the useEffect hook to change the title in the tab when you navigate away from the Users page.

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -3,14 +3,19 @@ import { Row, Col, Container } from 'react-bootstrap';
 import { useQuery } from '@apollo/client';
 import { QUERY_ALL_PROJECTS } from '../utils/queries';
 import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import Auth from '../utils/auth'
 
 export default  function Projects() {
+    const location = useLocation();
+
     useEffect(() => {
         if(Auth.loggedIn()){
             document.title = `${Auth.getProfile().data.username} is seeing all projects`
         } else { document.title = 'You are seeing all projects.'}
-        return;
+        return () => { 
+            if(location.pathname !== '/users') document.title = 'Unity Fund'
+        }
     }, [])
     const { data, loading } = useQuery(QUERY_ALL_PROJECTS);
 
