f0c0465f6699a58ea8a3ffc035f3f64ecfe5c6f6
Jonathan Maldonado Guemarez <cantor.dedekind@gmail.com>
2023-12-11 20:48:46 -0500

    Trying to use dotenv in ES module(import syntax) to see if that solves the problem with vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import 'dotenv/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 10000,
+    port: process.env.PORT || 3000,
     open: true,
     proxy: {
       '/graphql': {
